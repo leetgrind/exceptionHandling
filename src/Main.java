c46a92388333a9778946201d6b303dada7e8d61a
leetgrind <leetgrind.org@gmail.com>
2023-11-01 17:56:54 -0400

    added exception

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            System.out.println("Enter a number");
+
+            int number = 0;
+
+            if(number == 0) {
+                throw new IllegalArgumentException("Number cannot be zero");
+            }
+        }
+        catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+        catch (Exception ex) {
+            System.out.println("Generic exception");
+        }
+
     }
 }
